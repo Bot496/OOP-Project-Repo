9f44862b164f1571f2cc7b7175895ec1bd7563e8
KOH DA WEI <kohdawei25@gmail.com>
2025-09-24 00:47:37 +0800

    Final

--- Freight.cpp
+++ Freight.cpp
@@ -1,1 +1,30 @@
 #include "Freight.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void System::setDest(string d) {
+	dest = d;
+}
+
+void System::setTime(int t) {
+	time = t;
+}
+
+void System::setId(string num) {
+	id = num;
+}
+
+string System::getDest() const {
+	return dest;
+}
+
+int System::getTime() const {
+	return time;
+}
+
+string System::getId() const {
+	return id;
+}
+
